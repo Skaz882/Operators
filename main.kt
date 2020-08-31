6994a5eaac2c3cd9e783def55e88a966d296947f
Skaz882 <60199239+Skaz882@users.noreply.github.com>
2020-08-31 16:59:03 -0500

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val a = 12
+    val b = 6
+    val c = 3
+    val d = 1
+
+    val ans1 = c + d
+    print(ans1)
+
+    val ans2 = a - b
+    print(ans2)
+
+    val ans3 = b * c
+    print(ans3)
+
+    val ans4 = a / d
+    print(ans4)
+
+    val ans5 = b % c
+    print(ans5)
+
+    val isCompare = b > d
+    print(isCompare)
+}
